a48847c7e727884d41f9127953d1a250b0696af6
Marshal <marshalmartin002@gmail.com>
2025-01-12 08:57:01 +0000

    newwwwww

--- mex.js
+++ mex.js
@@ -21,7 +21,7 @@ dateElement.textContent = formattedDate;
 
 // Exchange rates
 const exchangeRates = {
-    "NGN": { "NGN": 1, "GHS": 0.0094},
+    "NGN": { "NGN": 1, "GHS": 0.0093},
     "GHS": { "GHS": 1, "NGN": 100}
 };
 
