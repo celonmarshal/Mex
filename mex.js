46794f660a36604a0127f349fbb3221ed205df30
Marshal <51497399+celonmarshal@users.noreply.github.com>
2025-03-06 08:33:25 +0000

    Update mex.jszxc

--- mex.js
+++ mex.js
@@ -21,8 +21,8 @@ dateElement.textContent = formattedDate;
 
 // Exchange rates
 const exchangeRates = {
-    "NGN": { "NGN": 1, "GHS": 0.0102},
-    "GHS": { "GHS": 1, "NGN": 92.10}
+    "NGN": { "NGN": 1, "GHS": 0.099},
+    "GHS": { "GHS": 1, "NGN": 94}
 };
 
 // Function to get the current date in readable format
