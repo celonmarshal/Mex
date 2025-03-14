be9b3c864e746e5920cdaa42370d913ff887aa8a
Marshal <marshalmartin002@gmail.com>
2025-03-14 11:02:48 +0000

    saee

--- mex.js
+++ mex.js
@@ -21,7 +21,7 @@ dateElement.textContent = formattedDate;
 
 // Exchange rates
 const exchangeRates = {
-    "NGN": { "NGN": 1, "GHS": 0.098},
+    "NGN": { "NGN": 1, "GHS": 0.0098},
     "GHS": { "GHS": 1, "NGN": 95.6}
 };
 
