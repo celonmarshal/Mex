e645aec90fca77ad263e00044a9e1ad5a770c07a
Marshal <51497399+celonmarshal@users.noreply.github.com>
2025-10-02 01:11:28 -0700

    Update mex.js

--- mex.js
+++ mex.js
@@ -22,7 +22,7 @@ dateElement.textContent = formattedDate;
 // Exchange rates
 const exchangeRates = {
     "NGN": { "NGN": 1, "GHS": 0.0089},
-    "GHS": { "GHS": 1, "NGN": 103.0}
+    "GHS": { "GHS": 1, "NGN": 102.0}
 };
 
 // Function to get the current date in readable format
@@ -64,6 +64,7 @@ function calculateAmountToSend() {
     document.getElementById('amountToSend').value = amountToSend.toFixed(2) + ' ' + fromCurrency;
     document.getElementById('calculationDate').innerText = `Calculation performed on: ${getCurrentDate()}`;
 }
+
 
 
 
