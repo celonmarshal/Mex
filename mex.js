edaabd0f3e6536ad51eb2d71cd8b5c1cdeee2987
Marshal <51497399+celonmarshal@users.noreply.github.com>
2025-10-20 00:53:15 -0700

    Fix currency conversion rates and update date format

--- mex.js
+++ mex.js
@@ -21,7 +21,7 @@ dateElement.textContent = formattedDate;
 
 // Exchange rates
 const exchangeRates = {
-    "NGN": { "NGN": 1, "GHS": 0.0077},
+    "NGN": { "NGN": 1, "GHS": 0.0076},
     "GHS": { "GHS": 1, "NGN": 120.0}
 };
 
@@ -64,6 +64,7 @@ function calculateAmountToSend() {
     document.getElementById('amountToSend').value = amountToSend.toFixed(2) + ' ' + fromCurrency;
     document.getElementById('calculationDate').innerText = `Calculation performed on: ${getCurrentDate()}`;
 }
+
 
 
 
